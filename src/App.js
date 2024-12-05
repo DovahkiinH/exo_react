3a5ec18e670ce3ca38762347834b02071196c5fa
DovahkiinH <loick.caplier@gmail.com>
2024-12-05 14:39:37 +0100

    ExReact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import {React, useState, useEffect} from 'react';
 
+import DataTable from "react-data-table-component";
+
 const App = (props) => {
 
   const [nom, setNom] = useState("");
 
   const [prenom, setPrenom] = useState("");
+
+  const [compter, setCompter] = useState(0);
+
+  const [liste, setListe] = useState([]);
+
+  const [nvelement, setNvElement] = useState("");
 
   useEffect(() => {
       console.log("useEffect 2 ...")
@@ -24,6 +32,24 @@ const App = (props) => {
 
   }
 
+  const handleChangeCompter = (evt) => {
+    setCompter(compter + 1);
+
+  }
+
+  const AE = () => {
+    if (nvelement.trim() !== ""){
+      setListe([...liste, nvelement]);
+      setNvElement("");
+
+    }
+  };
+
+  const handleChangeListe = (evt) => {
+    setListe(evt.target.value);
+
+  }
+
   console.log("render App...");
 
   return (
@@ -38,6 +64,28 @@ const App = (props) => {
              Bonjour {nom} {prenom}
 
           </div>
+
+          <br></br>
+
+          <button onClick={handleChangeCompter}>
+
+            Compteur = {compter}
+
+          </button>
+
+          <br></br>
+
+          <input type="text" placeholder='Element à ajouter...' value={nvelement} onChange={handleChangeListe}/><button onClick={AE}>
+          
+          Ajouter
+
+          </button>
+
+          <ul>
+          {liste.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+          </ul>
 
       </div>
   );
